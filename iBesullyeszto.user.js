c3c96365e7532ac343d21b7213ca40c36e81eec6
Neo <320219556+NeoCodesRed@users.noreply.github.com>
2026-08-23 20:52:28 +0200

    Update metadata to use GH

--- iBesullyeszto.user.js
+++ iBesullyeszto.user.js
@@ -2,9 +2,12 @@
 // @name        iBesullyeszto
 // @description Kiemelt termékek helyett pragmatikus, ár szerinti rendezés.
 // @include     /^https?://(www\.)?ipon\.hu/shop.*$/
-// @icon        https://i.imgur.com/62WJRj4.png
-// @version     1.0
 // @grant       none
+// @icon        https://raw.githubusercontent.com/NeoCodesRed/iBesullyeszto/refs/heads/main/iBesullyeszto_icon.png
+// @downloadURL https://raw.githubusercontent.com/NeoCodesRed/iBesullyeszto/refs/heads/main/iBesullyeszto.user.js
+// @updateURL   https://raw.githubusercontent.com/NeoCodesRed/iBesullyeszto/refs/heads/main/iBesullyeszto.user.js
+// @version     1.0
+// @author      Neo
 // ==/UserScript==
 
 
